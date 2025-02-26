4d810c35349c93af8d8c276ac4df6f8e2f66f6f1
Zuka Mzhavanadze <zukapuka123123@gmail.com>
2025-02-26 19:30:27 +0400

    new

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
-import { Component } from '@angular/core';
+
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { ProductsComponent } from './products/products.component';
+import { FooterComponent } from './footer/footer.component';
 
 export const routes: Routes = [
     {path: "", component : HomeComponent },
-    {path: "products", component : ProductsComponent }
+    {path: "products", component : ProductsComponent },
+  
 ];
--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,12 +1,20 @@
+import { CommonModule, NgClass } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
+
 @Component({
   selector: 'app-navbar',
-  imports: [RouterModule],
+  imports: [RouterModule, CommonModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
+
+  navbar: boolean = false
+
+  switch(){
+    this.navbar = !this.navbar
+  }
 
 }
