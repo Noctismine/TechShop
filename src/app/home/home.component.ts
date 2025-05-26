0722ba2920053a73fc6f393d039acb9dea940fa6
Zuka Mzhavanadze <zukapuka123123@gmail.com>
2025-05-27 00:33:18 +0400

    text changed

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { ApiService } from '../api.service';
 import { RouterLink } from '@angular/router';
 
@@ -6,23 +6,18 @@ import { RouterLink } from '@angular/router';
   selector: 'app-home',
   imports: [RouterLink],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrl: './home.component.css',
 })
 export class HomeComponent {
-
-  constructor(public api: ApiService){
-   this.getCards()
+  constructor(public api: ApiService) {
+    this.getCards();
   }
 
   cards: any = [];
- 
-  
- 
-    getCards(){
-      this.api.getCardById(1 , 10 ).subscribe(  (data) =>  {
-          
-          this.cards = data
-      })
-    }
 
+  getCards() {
+    this.api.getCardById(1, 10).subscribe((data) => {
+      this.cards = data;
+    });
+  }
 }
